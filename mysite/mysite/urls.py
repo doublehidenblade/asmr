c9bf1014e34ca387a1f78856cf1bf03fd0b13a3e
Dingyu Peng <dpeng4@illinois.edu>
2019-06-27 11:47:40 +0800

    asmr to ''

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('asmr/', include('asmr.urls')),
+    path('', include('asmr.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
